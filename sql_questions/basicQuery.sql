4495f4dd6bb08bde52967f76a25198bce4d4a84e
Stefan Washington <quanzhangubc2019@gmail.com>
2022-08-11 02:14:35 -0700

    queries

---
+++ sql_questions/basicQuery.sql
@@ -0,0 +1,16 @@
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+select name
+from students 
+where marks>75
+order by SUBSTR(name, LENGTH(name)-2, 3) , id asc;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+select name
+from employee 
+order by name asc;
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than 2000 per month who have been employees for less than  months. Sort your result by ascending employee_id.
+select name
+from employee
+where salary > 2000 AND months < 10
+order by employee_id asc;
--- sql_questions/selectById.sql
+++ sql_questions/selectById.sql
@@ -3,3 +3,40 @@ SELECT * FROM CITY WHERE ID = 1661
 SELECT * FROM CITY WHERE COUNTRYCODE = "JPN"
 
 SELECT NAME FROM CITY WHERE COUNTRYCODE = "JPN"
+
+SELECT CITY, STATE FROM STATION
+
+-- Find unique CITY entries with even id number in the table.
+SELECT DISTINCT(CITY) FROM STATION WHERE MOD(ID, 2) = 0;
+
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT (COUNT(CITY) - COUNT(DISTINCT CITY)) FROM STATION;
+
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+select city, length(city) from station
+order by length(city),city asc
+limit 1;
+select city, length(city) from station
+order by length(city) desc
+limit 1;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT city FROM station WHERE LOWER(SUBSTR(CITY,1,1)) in ('a','e','i','o','u');
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY,LENGTH(CITY),1)) IN ('a','e','i','o','u');
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LOWER(SUBSTR(CITY,LENGTH(CITY),1)) IN ('a','e','i','o','u') 
+AND LOWER(SUBSTR(CITY,1,1)) IN ('a','e','i','o','u');
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT city FROM station WHERE LOWER(SUBSTR(CITY,1,1)) not in ('a','e','i','o','u');
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE LOWER(SUBSTR(CITY,LENGTH(CITY),1)) NOT IN ('a','e','i','o','u') 
+OR LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u');
