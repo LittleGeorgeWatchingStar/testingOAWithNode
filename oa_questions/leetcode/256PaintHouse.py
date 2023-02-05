68218375346fde3ebf3c0c1d480e20ca53958189
stevenzhang <quanzhangubc2019@gmail.com>
2023-02-05 09:56:41 -0800

    prepare

---
+++ oa_questions/leetcode/256PaintHouse.py
@@ -0,0 +1,21 @@
+#256PaintHouse
+#dynamic programming
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param costs: n x 3 cost matrix
+    @return: An integer, the minimum cost to paint all houses
+    """
+    def min_cost(self, costs: List[List[int]]) -> int:
+        # write your code here
+        dp = [0, 0, 0]
+        for i in range(len(costs)):
+            dp0 = costs[i][0] + min(dp[1], dp[2])
+            dp1 = costs[i][1] + min(dp[0], dp[2])
+            dp2 = costs[i][2] + min(dp[0], dp[1])
+            dp = [dp0, dp1, dp2]
+
+        return min(dp)
